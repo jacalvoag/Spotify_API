ef9e391cc2a6208db0341f231401f69c50b41b00
Jose Calvo <jacalvoaguilar@gmail.com>
2025-10-27 22:27:16 -0600

    Se hace el CSS de Player y se importa a Layout

--- src/app/layout/layout.ts
+++ src/app/layout/layout.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Sidebar } from '../shared/components/sidebar/sidebar';
+import { Player } from '../shared/components/player/player';
 
 @Component({
     selector: 'app-layout',
     standalone: true,
-    imports: [RouterOutlet, Sidebar],
+    imports: [RouterOutlet, Sidebar, Player],
     templateUrl: './layout.html',
     styleUrl: './layout.css',
     changeDetection: ChangeDetectionStrategy.OnPush,
