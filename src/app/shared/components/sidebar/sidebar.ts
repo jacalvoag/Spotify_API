2f6ed2e26d2908fd3ea4ed4d099e5d122cb65890
Jose Calvo <jacalvoaguilar@gmail.com>
2025-10-27 21:56:29 -0600

    Añadimos el componente de SideBar con su estructura HTML basica sin CSS de momento

---
+++ src/app/shared/components/sidebar/sidebar.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './sidebar.html',
+  styleUrl: './sidebar.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Sidebar {}
